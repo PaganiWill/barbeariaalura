c9d8e71146dbf534edab58bc02795876de10b7c6
Pagani <williamlg_pagani@hotmail.com>
2022-10-31 23:02:34 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>  
+
+void main ()
+{
+   printf("%s","Isto é uma string ....\n"); // note o '\n' no final da string;
+   printf("%s","Outra string ...."); 
+   printf("%s","Terceira string\n"); 
+
+}
